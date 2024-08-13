0f547f3d81634a9e02410f47611b26d84efebd34
majidali137 <majidaliwp69@gmail.com>
2024-08-13 20:26:31 +0500

    removed some padding and margin for mobile

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -11,9 +11,9 @@ const page = () => {
                 <Carousel />
             </div>
 
-            <div className="text-white relative px-12  max-w-2xl mx-auto  mt-10" >
-                <h1 className='mb-2 text-center text-[45px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[30px] md:mb-5 md:text-[40px] lg:text-[60px]'>Website Redesigns</h1>
-                <p className='mt-4 mb-5 sm:mt-0 text-left text-[18px] sm:text-[20px] md:mb-10 md:text-[20px] lg:text-[20px] font-medium'> Welcome to our ultimate results library! Included below are featured website redesigns that Power Funnels has designed for our clients in all types of industries, from: Blockchain, E-Commerce, Medical & more!</p>
+            <div className="text-white relative px-8  max-w-2xl mx-auto  mt-10" >
+                <h1 className='mb-2 text-center text-[35px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[30px] md:mb-5 md:text-[40px] lg:text-[60px]'>Website Redesigns</h1>
+                <p className='mt-4 mb-5 sm:mt-0 text-left text-[16px] sm:text-[20px] md:mb-10 md:text-[20px] lg:text-[20px] font-medium'> Welcome to our ultimate results library! Included below are featured website redesigns that Power Funnels has designed for our clients in all types of industries, from: Blockchain, E-Commerce, Medical & more!</p>
             </div>
 
 
